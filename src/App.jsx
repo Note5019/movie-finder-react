c216edee8fe14231bb14fdfac820c1c66141f654
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2025-04-18 17:54:09 +0700

    Add search movie and main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,130 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from "react";
+import Search from "./components/Search.jsx";
+import {useDebounce} from "react-use";
+import MovieCard from "./components/MovieCard.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Card = ({title}) => {
+    const [hasLiked, setHasLiked] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        console.log(`${title} has been liked: ${hasLiked}`)
+    });
+
+    return (
+        <div className="card">
+            <h2>{title}</h2>
+
+            <button onClick={() => setHasLiked(!hasLiked)}>
+                {hasLiked ? "Liked" : "Like"}
+            </button>
+        </div>
+    );
+};
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
 }
 
-export default App
+const App = () => {
+    const [searchTerm, setSearchTerm] = useState("")
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("")
+    const [movieList, setMovieList] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const fetchMovies = async (query = '') => {
+        setIsLoading(true)
+        setErrorMessage('')
+
+        try {
+            const endpoint = query
+                ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const response = await fetch(endpoint, API_OPTIONS)
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch movies')
+            }
+
+            const data = await response.json()
+            console.log('data', data)
+            if (data.Response === 'False') {
+                setErrorMessage(data.Error || 'Failed to fetch movies')
+                setMovieList([])
+            }
+
+            setMovieList(data.results || [])
+            console.log('movieList', movieList)
+        } catch (error) {
+            console.error(error)
+            setErrorMessage('Error fetching movies. Please try again later.')
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+    useEffect(() => {
+        fetchMovies(debouncedSearchTerm)
+    }, [debouncedSearchTerm])
+
+    return (
+        <main>
+            <div className="pattern"/>
+
+            <div className="wrapper">
+                <header>
+                    <img src="./hero.png" alt="Movies"/>
+                    <h1>Find <span className="text-gradient">Movies</span> You&#39;ll Love Without the Hassle</h1>
+
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                    <span className="text-white">{searchTerm}</span>
+                </header>
+
+                <section className="trending">
+                    trending
+                </section>
+
+                <section className="all-movies">
+                    <h2>Popular</h2>
+                    <ul>
+                        {
+                            movieList.map(movie => (<MovieCard key={movie.id} movie={movie}/>))
+                        }
+                    </ul>
+                </section>
+            </div>
+        </main>
+        // <div className="bg-[url(hero-bg.png)]">
+        //     <header className="">
+        //         <div className="flex flex-col justify-center items-center">
+        //
+        //
+        //
+        //         </div>
+        //
+        //     </header>
+        //     {/*<div className="pt-10 flex flex-col justify-center items-center">*/}
+        //     {/*    */}
+        //     {/*</div>*/}
+        //
+        //
+        //
+        //     <div className="card-container">
+        //         <Card title="Star Wars" rating={5} isCool={true} />
+        //         <Card title="Avatar" />
+        //         <Card title="The Lion King" />
+        //         <div className="bg-red-500 w-10 h-10"></div>
+        //     </div>
+        // </div>
+    );
+};
+
+export default App;
